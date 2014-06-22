980f913f42604d625d44ea3cddb16cdea95967fd
Friedrich Lindenberg <friedrich@pudo.org>
2014-06-22 11:26:23 -0400

    simple post interface

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require('express'),
     messages = require('./messages'),
-    _ = require('underscore'),
-    async = require('async');
+    async = require('async'),
+    bodyParser = require('body-parser')
     
 
 var app = express();
 app.use(express.static(__dirname + '/static'));
+app.use(bodyParser.json())
 
 messages.init();
 
@@ -14,17 +15,16 @@ app.get('/messages', function(req, res) {
     async.map(results, messages.read, function(err, results) {
       var data = {'results': results, 'total': total};
       res.jsonp(data);
-      //res.render('index', data);
     });
   });
 });
 
-app.get('/xx', function(req, res) {
-  messages.traverse(25, 0, function(results, total) {
-    async.map(results, messages.read, function(err, results) {
-      var data = {'results': results, 'total': total};
-      res.render('index', data);
-    });
+app.post('/messages', function(req, res) {
+  var message = req.body;
+  message.created_at = new Date();
+  messages.create(message, function(id) {
+    message.id = id;
+    res.jsonp(message);
   });
 });
 
